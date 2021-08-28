2de58772c4b0b32287240284adc4351fd3d5cb7f
Krister Ian Arquiola <kristerian@satellite.com.ph>
2021-08-28 21:02:35 +0800

    move bin/bash

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,10 @@
+#!/bin/bash
 
 #change me
 export AWS_ACCESS_KEY_ID="AKIA2JW3OZSUCEZURAJ5"
 export AWS_DEFAULT_REGION="ca-central-1"
 export AWS_SECRET_ACCESS_KEY="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"
 
-
-
-
-#Begin script
-
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/$release/hashicorp.repo
-sudo yum install -y terraform
-
-
-
-#!/bin/bash
 
 case "$1" in 
 create)
